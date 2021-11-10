b479d3d007f47fef5e92110931da12fceb4a5395
Andrea Serpolla <a.serpolla@gmail.com>
2021-11-10 18:09:15 +0100

    admin access

---
+++ helpers/IAM.py
@@ -0,0 +1,37 @@
+
+import pandas as pd
+from typing import Union
+
+
+class IAM:
+
+    _credentials: pd.DataFrame = pd.DataFrame()
+
+
+    def __init__(self, credentials: Union[pd.DataFrame,dict]):
+
+        # parse dict
+        if type(credentials) == dict:
+            credentials = pd.DataFrame(credentials)
+
+        # validate dataframe
+        for col in ['username', 'password']:
+            if col not in credentials.columns.tolist():
+                raise ValueError('invalid credentials')
+
+        self._credentials = credentials
+
+
+    def auth(self, username: str, password: str) -> bool:
+
+        '''Return True if user is registered and credentials are correct,
+           False otherwise'''
+
+        # look for user
+        if username not in self._credentials.username.tolist():
+            return False
+
+        # check password
+        return password == self._credentials.loc[lambda x: x.username == username].password.values[0]
+
+### class IAM ###
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 import helpers.Answer
+import helpers.IAM
 
 import pandas as pd
 import streamlit as st
+import os
 import sys
 import logging
 
@@ -13,49 +15,104 @@ N_SEATS = 43
 
 def main(path: str = './prenotazioni.csv') -> None:
 
-    st.set_page_config(page_title=TITLE)
+    st.set_page_config(page_title=TITLE, initial_sidebar_state='collapsed')
 
     st.title(TITLE)
     st.markdown(' ')
 
-    with st.form('main_form'):
+    # init IAM
+    iam = helpers.IAM.IAM(st.secrets['credentials'])
+    admin = False
 
-        answer = helpers.Answer.Answer(path, N_SEATS)
+    # admin login
 
-        col1, col2 = st.columns(2)
-        with col1:
-            answer.name = st.text_input(label='Nome', key='name_input')
-        with col2:
-            answer.surname = st.text_input(label='Cognome', key='surname_input')
+    st.sidebar.header('Accedi')
+    username = st.sidebar.text_input(label='Username', key='username_input')
+    password = st.sidebar.text_input(label='Password', type='password', key='password_input')
 
-        col1, col2 = st.columns(2)
-        with col1:
-            answer.email = st.text_input(label='Email', key='email_input')
-        with col2:
-            answer.phone = st.text_input(label='Numero di telefono', key='phone_input')
+    # login button
+    if st.sidebar.button('Login', key='login_button'):
+        # check credentials
+        admin = iam.auth(username, password)
+        if not admin:
+            st.sidebar.error('Credenziali errate!')
 
-        answer.seat = st.selectbox(label='Posto', options=answer.get_available_seats(), key='seat_select')
-        answer.agree = st.checkbox(label='Consenso al trattamento dei dati')
+    # main page
 
-        if st.form_submit_button('Prenota'):
+    if not admin:
 
-            response = answer.save()
+        with st.form('main_form'):
 
-            if type(response) == str:
-                # some field is not valid
-                if response == 'name' or response == 'surname':
-                    st.error('Nome e cognome non sono validi')
-                if response == 'email':
-                    st.error('Inserire un indirizzo email valido')
-                if response == 'phone':
-                    st.error('Inserire un numero di telefono valido')
-                if response == 'seat':
-                    st.error('Il posto scelto è già occupato')
-                    st.info('Ricarica la pagina per aggiornare la lista dei posti disponibile')
-                if response == 'agree':
-                    st.error('Il consenso al trattamento dei dati personali è obbligatorio')
-            else:
-                st.success('Prenotazione correttamente registrata')
+            answer = helpers.Answer.Answer(path, N_SEATS)
+
+            col1, col2 = st.columns(2)
+            with col1:
+                answer.name = st.text_input(label='Nome', key='name_input')
+            with col2:
+                answer.surname = st.text_input(label='Cognome', key='surname_input')
+
+            col1, col2 = st.columns(2)
+            with col1:
+                answer.email = st.text_input(label='Email', key='email_input')
+            with col2:
+                answer.phone = st.text_input(label='Numero di telefono', key='phone_input')
+
+            answer.seat = st.selectbox(label='Posto', options=answer.get_available_seats(), key='seat_select')
+            answer.agree = st.checkbox(label='Consenso al trattamento dei dati')
+
+            if st.form_submit_button('Prenota'):
+
+                response = answer.save()
+
+                if type(response) == str:
+                    # some field is not valid
+                    if response == 'name' or response == 'surname':
+                        st.error('Nome e cognome non sono validi')
+                    if response == 'email':
+                        st.error('Inserire un indirizzo email valido')
+                    if response == 'phone':
+                        st.error('Inserire un numero di telefono valido')
+                    if response == 'seat':
+                        st.error('Il posto scelto è già occupato')
+                        st.info('Ricarica la pagina per aggiornare la lista dei posti disponibile')
+                    if response == 'agree':
+                        st.error('Il consenso al trattamento dei dati personali è obbligatorio')
+                else:
+                    st.success('Prenotazione correttamente registrata')
+
+    else: # user is admin
+
+        # show logout button
+        st.sidebar.button('Logout')
+
+        # show prenotations
+        if os.path.exists(path):
+
+            # print prenotations
+            df = pd.read_csv(path)
+            st.dataframe(df)
+
+            # convert df to csv
+            @st.cache
+            def get_csv(df: pd.DataFrame) -> str:
+                return df.to_csv(index=False).encode('utf-8')
+
+            # download df
+
+            # [BUG]
+            # pressing download button logout admin automatically.
+            # a form does not solve the problem because download buttons
+            # can not be used in forms
+
+            st.download_button(label='Download',
+                               data=get_csv(df),
+                               file_name=f'{pd.Timestamp.utcnow().tz_convert("Europe/Rome").strftime("%Y-%m-%d_%H.%M.%S")}.csv',
+                               key='download_button'
+                              )
+
+        # no prenotation to show
+        else:
+            st.info('Non è stata registrata alcuna prenotazione ancora')
 
 ### main ###
 
