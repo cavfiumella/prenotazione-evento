adfa986046d381447761f600419f20e402e74176
Andrea Serpolla <a.serpolla@gmail.com>
2021-12-03 15:25:17 +0100

    minor: Logbook code layout improvements

--- helpers/Logbook.py
+++ helpers/Logbook.py
@@ -1,26 +1,26 @@
 
-"""Logbook of admins operations."""
-
 import helpers.time
 
 
 class Logbook:
 
-    _path: str = None
+    """Logbook of admins operations."""
+
+    __path: str = "logbook"
 
 
-    def __init__(self, path: str = 'book.log'):
-        self._path = path
+    def __init__(self, path: str = "logbook"):
+        self.__path = path
 
 
     def log(self, s: str):
-        s = helpers.time.format(helpers.time.now()) + ' - ' + s + '\n'
-        with open(self._path, mode='a') as file:
+        s = f"{helpers.time.format(helpers.time.now())} - {s}\n"
+        with open(self.__path, mode="a") as file:
             file.write(s)
 
 
     def read(self) -> str:
-        with open(self._path, mode='r') as file:
+        with open(self.__path, mode="r") as file:
             return file.read()
 
 ### class Logbook ###
